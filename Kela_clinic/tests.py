bb73e0ad43a191bbe541e1a24c8fd9a85d06d45b
Andrey Ruch <73066767+Andrey-Ruch@users.noreply.github.com>
2022-03-28 20:02:21 +0300

    Update tests.py

--- Kela_clinic/tests.py
+++ Kela_clinic/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django import urls
+from django.test import TestCase,SimpleTestCase
+from django.urls import resolve, reverse
+import unittest
 
 # Create your tests here.
+class TestUrls(unittest.TestCase):
+
+    def test_list_url_is_resolved(self):
+        url=reverse('index')
+        print("HOME PAGE TEST ++++++++++++++>")
+        print(resolve(url))
