69bd4841be6987e343e9f297ee49f408b6f60731
KevynKrancen <102467763+KevynKrancen@users.noreply.github.com>
2022-04-01 14:20:56 +0300

    Update tests.py

--- Kela_clinic/tests.py
+++ Kela_clinic/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.urls import resolve, reverse
+from Kela_clinic.views import *
+import unittest
+from django.test.client import RequestFactory
 
+
+
+class TestUrls(unittest.TestCase):
+
+    def test_list_url_is_resolved(self):
+        url=reverse('index')
+        print(resolve(url))
 # Create your tests here.
