9a9d6346ba23a9e95500cad41abf15565d359af5
elad770 <elad.yh10@walla.co.il>
2022-04-24 22:08:38 +0300

    BSPM2022T16-56 Sending prescriptions

--- Kela_clinic/models.py
+++ Kela_clinic/models.py
@@ -6,14 +6,24 @@ from django.contrib.auth.models import User
 # Create your models here.
 class PatientModel(models.Model):
     ID=models.CharField(db_column='ID',max_length=9,primary_key=True)
-    firstnames=models.TextField(db_column='firstname')
+    firstname=models.TextField(db_column='firstname')
     lastname=models.TextField(db_column='lastname')
     password=models.CharField(db_column='password',max_length=10)
     messagesent=models.TextField(db_column='messagesent')
     DIC=models.CharField(db_column='DID',max_length=9)
     medicalrecod=models.TextField(db_column='medicalrecod')
+  
+
+    def patient_update_status(user):
+        all_status = ["sick","very sick","healthy"]
+        if user!= None or user.status!="healthy" and user.status in all_status:
+            user.status = all_status[all_status.index(user.status) + 1]
+            return "status of user {0} update seucces".format(user.firstname)
+        return "status of user {0} update not seucces".format(user.firstname)
+
+
     class Meta:
-        managed = True
+        managed = False
         db_table = 'user'
 
 class DoctorModel(models.Model):
@@ -23,8 +33,32 @@ class DoctorModel(models.Model):
     password=models.CharField(db_column='password',max_length=10)
     patientID=models.CharField(db_column='PID',max_length=9)
     workday=models.TextField(db_column='workday')
+
+    def get_all_madicl():
+       return [
+           "Methylphenidate Hydrochloride",
+           "Glimepiride",
+           "Methocarbamol",
+           "Acetaminophen",
+           "Oasis TEARS PLUS",
+           "Selenium",
+           "Calcium Acetate"
+           "entresto",
+           "Etodolac",
+           "Ciclosporin",
+           "Lithium Bromatum",
+           "Flovent Diskus",
+           "Methyldopa",
+           "Tazorac",
+           "Linezolid",
+           "Losartan",
+           "ciprofloxacin"
+           "Amlodipine"
+           "Eszopiclone",
+            ]
+
     class Meta:
-        managed = True
+        managed = False
         db_table = 'doctor'
 
 
@@ -35,7 +69,7 @@ class Adminmodel(models.Model):
     password=models.CharField(db_column='password',max_length=10)
     messagesent=models.TextField(db_column='messagesent')
     class Meta:
-        managed = True
+        managed = False
         db_table = 'admin'
 
 
