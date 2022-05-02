da6846e7149a7de6b18f8602aeaed668b91d302f
elad770 <elad.yh10@walla.co.il>
2022-05-02 20:36:47 +0300

    BSPM2022T16-59 commit59

--- projectapp/views.py
+++ projectapp/views.py
@@ -32,7 +32,6 @@ cursor = db_connection.cursor()
 print(db_connection)
 # START PAGE WITH ANIMATION
 """
-test test test 4343 5423653
 medsnames =  ["Methylphenidate Hydrochloride",
                 "Glimepiride",
                 "Methocarbamol",
